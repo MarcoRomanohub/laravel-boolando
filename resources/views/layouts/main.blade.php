250768fd129f3e5bbd64a9604a75295bc0511144
MarcoRomanoHub <marcoromano270@gmail.com>
2024-05-09 17:12:28 +0200

    [add] partials

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,6 +14,16 @@
 
 <body>
 
+    @include('partials.header')
+
+    <main>
+
+        @yield('content')
+
+    </main>
+
+    @include('partials.footer')
+
 </body>
 
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="topbar container d-flex">
+        <div class="categorie d-flex">
+            <a href="#" class="top-link"> Home </a>
+            <a href="#" class="top-link"> Products </a>
+            <a href="#" class="top-link"> Contacts </a>
+        </div>
+        <div class="logo"><img src="/img/boolean-logo.png" alt="Logo"></div>
+        <div class="area-personale">
+            <i class="fa-regular fa-user"></i>
+            <i class="fa-regular fa-heart"></i>
+            <i class="fa-sharp fa-solid fa-bag-shopping"></i>
+        </div>
+    </div>
+</header>
