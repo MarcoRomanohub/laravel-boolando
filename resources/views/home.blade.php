c85cfcdb2ccf90adc77bac476afd1cd99af8b3b8
MarcoRomanoHub <marcoromano270@gmail.com>
2024-05-09 17:49:09 +0200

    [fixed] cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,59 +1,72 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="box-product">
-        <div class="first-img">
-            <img src="/img/1.webp" alt="">
+    <div class="container d-flex">
+        <div class="box-product">
+            <div class="first-img">
+                <img src="/img/1.webp" alt="">
+            </div>
+            <div class="marchio"> brand </div>
+            <h4> Name </h4>
+            <div class="price">14,99 € <span> FullPrice </span></div>
+            <div class="badge.type" class="discount"> badge . value </div>
+            <div class="sostenibile"> Tag </div>
+            <div class="heart">&hearts;</div>
         </div>
-        <div class="marchio"> brand </div>
-        <h4> Name </h4>
-        <div class="price">14,99 € <span> FullPrice </span></div>
-        <div class="badge.type" class="discount"> badge . value </div>
-        <div class="sostenibile"> Tag </div>
-        <div class="heart">&hearts;</div>
-    </div>
-    <div class="box-product">
-        <div class="first-img">
-            <img src="/img/1.webp" alt="">
+        <div class="box-product">
+            <div class="first-img">
+                <img src="/img/1.webp" alt="">
+            </div>
+            <div class="marchio"> brand </div>
+            <h4> Name </h4>
+            <div class="price">14,99 € <span> FullPrice </span></div>
+            <div class="badge.type" class="discount"> badge . value </div>
+            <div class="sostenibile"> Tag </div>
+            <div class="heart">&hearts;</div>
         </div>
-        <div class="marchio"> brand </div>
-        <h4> Name </h4>
-        <div class="price">14,99 € <span> FullPrice </span></div>
-        <div class="badge.type" class="discount"> badge . value </div>
-        <div class="sostenibile"> Tag </div>
-        <div class="heart">&hearts;</div>
-    </div>
-    <div class="box-product">
-        <div class="first-img">
-            <img src="/img/1.webp" alt="">
+        <div class="box-product">
+            <div class="first-img">
+                <img src="/img/1.webp" alt="">
+            </div>
+            <div class="marchio"> brand </div>
+            <h4> Name </h4>
+            <div class="price">14,99 € <span> FullPrice </span></div>
+            <div class="badge.type" class="discount"> badge . value </div>
+            <div class="sostenibile"> Tag </div>
+            <div class="heart">&hearts;</div>
         </div>
-        <div class="marchio"> brand </div>
-        <h4> Name </h4>
-        <div class="price">14,99 € <span> FullPrice </span></div>
-        <div class="badge.type" class="discount"> badge . value </div>
-        <div class="sostenibile"> Tag </div>
-        <div class="heart">&hearts;</div>
-    </div>
-    <div class="box-product">
-        <div class="first-img">
-            <img src="/img/1.webp" alt="">
+        <div class="box-product">
+            <div class="first-img">
+                <img src="/img/1.webp" alt="">
+            </div>
+            <div class="marchio"> brand </div>
+            <h4> Name </h4>
+            <div class="price">14,99 € <span> FullPrice </span></div>
+            <div class="badge.type" class="discount"> badge . value </div>
+            <div class="sostenibile"> Tag </div>
+            <div class="heart">&hearts;</div>
         </div>
-        <div class="marchio"> brand </div>
-        <h4> Name </h4>
-        <div class="price">14,99 € <span> FullPrice </span></div>
-        <div class="badge.type" class="discount"> badge . value </div>
-        <div class="sostenibile"> Tag </div>
-        <div class="heart">&hearts;</div>
-    </div>
-    <div class="box-product">
-        <div class="first-img">
-            <img src="/img/1.webp" alt="">
+        <div class="box-product">
+            <div class="first-img">
+                <img src="/img/1.webp" alt="">
+            </div>
+            <div class="marchio"> brand </div>
+            <h4> Name </h4>
+            <div class="price">14,99 € <span> FullPrice </span></div>
+            <div class="badge.type" class="discount"> badge . value </div>
+            <div class="sostenibile"> Tag </div>
+            <div class="heart">&hearts;</div>
         </div>
-        <div class="marchio"> brand </div>
-        <h4> Name </h4>
-        <div class="price">14,99 € <span> FullPrice </span></div>
-        <div class="badge.type" class="discount"> badge . value </div>
-        <div class="sostenibile"> Tag </div>
-        <div class="heart">&hearts;</div>
+        <div class="box-product">
+            <div class="first-img">
+                <img src="/img/1.webp" alt="">
+            </div>
+            <div class="marchio"> brand </div>
+            <h4> Name </h4>
+            <div class="price">14,99 € <span> FullPrice </span></div>
+            <div class="badge.type" class="discount"> badge . value </div>
+            <div class="sostenibile"> Tag </div>
+            <div class="heart">&hearts;</div>
+        </div>
     </div>
 @endsection
